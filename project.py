198e7259ab8d9551ebbe595abfe4249e7a9d6806
poudelpawan770-lab <poudelpawan770@gmail.com>
2025-09-24 16:06:28 +0545

    slicing part in python

---
+++ project.py
@@ -0,0 +1,11 @@
+#calculating the marks of student according to theifr performance 
+mark1 = 91
+if mark1 >= 90:
+    print("Congrulations!!! you have scored 'A'")
+elif mark1 >= 80 and mark1 < 90:
+    print("Good you have scored 'B' in your results.")
+elif mark1 >= 70 and mark1 <80:
+    print("you have scored 'C' Try to grab good marks.")
+else:
+    print("You are fail and your score is 'D' Work hard.")
+    print("The grade of the student is->", mark1)
