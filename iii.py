c0a0c1a61bb890a162acd442450ad640ab93198e
poudelpawan770-lab <poudelpawan770@gmail.com>
2025-09-24 07:35:57 +0545

    Refactor code to demonstrate string indexing and slicing in Python

--- iii.py
+++ iii.py
@@ -1,16 +1,16 @@
-import random
-
-number_to_guess = random.randint(1, 100)
-attempts = 0
-
-while True:
-    guess = int(input("Guess a number between 1 and 100: "))
-    attempts += 1
-
-    if guess < number_to_guess:
-        print("Too low!")
-    elif guess > number_to_guess:
-        print("Too high!")
-    else:
-        print(f"Congratulations! You guessed the number in {attempts} attempts.")
-        break
+#indexing 
+name = "pawan poudel"
+ch = name[3]
+print(ch)
+#slicing in python
+has_sentence = "python is amazing."
+char = has_sentence[0:17]#[0:17]
+print(char)
+print(has_sentence[:4])#[1:4]
+print(has_sentence[1:])#[2:len(str)]
+print(has_sentence[9:len(has_sentence)])#[9:[len(str)]]
+#negative slicing
+has_id = "pawan poudel"
+new_char = has_id[-5]
+print(new_char)
+print(has_id[-9:-1])#always take from the lowest to highest while slicing in python 
